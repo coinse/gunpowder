3ad194d8a150be47fcdd36336de9a2e062a700e0
Junhwi Kim <junhwi.kim23@gmail.com>
2016-12-26 18:33:10 +0900

    Print branch distance

--- util/branchdistance.c
+++ util/branchdistance.c
@@ -1,36 +1,47 @@
+#include <math.h>
+#include "log.c"
+
+double const K = 1.0;
+
 int isGreater(int a, int b, int stmtid){
 
     int result = (a > b);
-    log(stmtid, result, branchdistance);
+    double branchdistance = b - a + K;
+    tracelog(stmtid, result, branchdistance);
     return result;
 }
 int isEqGreater(int a, int b, int stmtid){
 
     int result = (a >= b);
-    log(stmtid, result, branchdistance);
+    double branchdistance = b - a + K;
+    tracelog(stmtid, result, branchdistance);
     return result;
 }
 int isLess(int a, int b, int stmtid){
 
     int result = (a < b);
-    log(stmtid, result, branchdistance);
+    double branchdistance = a - b + K;
+    tracelog(stmtid, result, branchdistance);
     return result;
 }
 int isEqLess(int a, int b, int stmtid){
 
     int result = (a <= b);
-    log(stmtid, result, branchdistance);
+    double branchdistance = a - b + K;
+    tracelog(stmtid, result, branchdistance);
     return result;
 }
 int isEqual(int a, int b, int stmtid){
 
     int result = (a == b);
-    log(stmtid, result, branchdistance);
+    double branchdistance = abs(a - b) + K;
+    tracelog(stmtid, result, branchdistance);
     return result;
 }
 int isnotEqual(int a, int b, int stmtid){
 
     int result = (a != b);
-    log(stmtid, result, branchdistance);
+    double branchdistance = a == b ? K : 0.0;
+    tracelog(stmtid, result, branchdistance);
     return result;
 }
--- util/log.c
+++ util/log.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int tracelog(int stmtid, int result, int branchdistance) {
+int tracelog(int stmtid, int result, double branchdistance) {
     FILE *f = fopen("trace", "a");
-    fprintf(f, "%d %d %d\n", stmtid, result, branchdistance);
+    fprintf(f, "%d %d %f\n", stmtid, result, branchdistance);
     fclose(f);
 }
