685ac5b9a1b8e39c5867406b73b1fda763959b01
Junhwi Kim <junhwi.kim23@gmail.com>
2017-01-09 15:58:11 +0900

    Use cffi as an runtime interface

--- execution.py
+++ execution.py
@@ -1,17 +1,26 @@
-import clang
+import cavm
 import argparse
 from subprocess import Popen, PIPE
+from cffi import FFI
 
 parser = argparse.ArgumentParser(description='Instrument and execute given c file')
-parser.add_argument('target', metavar='file', type=str, help='location of target c file')
+parser.add_argument('target', metavar='<target file>', type=str, help='location of target c file')
+parser.add_argument('-f', '--function', metavar='<target function>', type=str, help='name of function', required=False)
 args = parser.parse_args()
 
-clang.instrument(args.target)
-print(args.target[:-2])
+if args.function:
+  p = cavm.Parser(args.target)
+  p.instrument(args.function)
 
-with Popen(['gcc', args.target[:-2]+'.inst.c', '-o', args.target[:-2]], stdout=PIPE) as proc:
-    print(proc.stdout.read())
+  # TODO: update to use cpp compiler
+  with Popen(['gcc', args.target[:-2]+'.inst.c', '-shared', '-o', args.target[:-2]+'.so'], stdout=PIPE) as proc:
+      print(proc.stdout.read())
 
-with Popen([args.target[:-2]], stdout=PIPE) as proc:
-    with open('trace', 'r') as f:
-      print(f.read())
+  ffi = FFI()
+  ffi.cdef(p.get_decl(args.function))
+  C = ffi.dlopen(args.target[:-2]+'.so')
+  getattr(C, args.function)(1,2,3)
+
+else:
+  # TODO: print out the list of functions in target code
+  print('list of functions')
