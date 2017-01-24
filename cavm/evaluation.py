630d7c8d3ac71ca87d4cd7ca190f9cd8f4cbcd50
Junhwi Kim <junhwi.kim23@gmail.com>
2017-01-24 14:20:49 +0900

    Run yapf

--- cavm/evaluation.py
+++ cavm/evaluation.py
@@ -1,10 +1,10 @@
 def get_trace(dynamic_lib):
-  trace = []
-  for i in range(dynamic_lib.getTraceSize()):
-    t = dynamic_lib.getTrace(i)
-    trace.append((t.stmtid, t.result, t.trueDistance, t.falseDistance))
-  dynamic_lib.resetTrace()
-  return trace
+    trace = []
+    for i in range(dynamic_lib.getTraceSize()):
+        t = dynamic_lib.getTrace(i)
+        trace.append((t.stmtid, t.result, t.trueDistance, t.falseDistance))
+    dynamic_lib.resetTrace()
+    return trace
 
 
 def get_dep_chain(dependency_map, targetbranch):
@@ -24,11 +24,12 @@ def get_divergence_point(trace, dependency_chain):
         dist = float("inf")
         for traceid in range(len(trace)):
             executed = trace[traceid]
-            if(executed[0] == dependency_node[0]):
-                if(executed[1] == dependency_node[1]):
+            if (executed[0] == dependency_node[0]):
+                if (executed[1] == dependency_node[1]):
                     return None
                 else:
-                    thisdist = executed[2] if dependency_node[1] else executed[3]
+                    thisdist = executed[2] if dependency_node[1] else executed[
+                        3]
                     if thisdist < dist:
                         dist = thisdist
                         closest = [traceid, depid, dependency_node[1]]
@@ -36,6 +37,7 @@ def get_divergence_point(trace, dependency_chain):
             if not closest == []:
                 return closest
     return [-1, -1, False]
+
 
 class ObjFunc:
     def __init__(self, target_ftn, dlib, ffi, cfg, p, d):
@@ -51,16 +53,16 @@ class ObjFunc:
     def vector_to_input(self, vector, idx, params):
         i = []
         for t in params:
-          if t in ['unsigned int', 'int', 'long', 'float', 'double']:
-            i.append(vector[idx])
-            idx += 1
-          elif t[-1:] == '*':
-            raise NotImplementedError
-          elif t[:6] == 'struct':
-            fields = self.decls[t][1]
-            val, idx = self.vector_to_input(vector, idx, fields)
-            # struct should be made using pointer and dereferenced
-            i.append(self.ffi.new(t+"*", val)[0])
+            if t in ['unsigned int', 'int', 'long', 'float', 'double']:
+                i.append(vector[idx])
+                idx += 1
+            elif t[-1:] == '*':
+                raise NotImplementedError
+            elif t[:6] == 'struct':
+                fields = self.decls[t][1]
+                val, idx = self.vector_to_input(vector, idx, fields)
+                # struct should be made using pointer and dereferenced
+                i.append(self.ffi.new(t + "*", val)[0])
         return (i, idx)
 
     def set_target(self, branch_id):
@@ -90,9 +92,10 @@ class ObjFunc:
                 branch_dist = float("inf")
             else:
                 app_lv = divpoint[1]
-                branch_dist = trace[divpoint[0]][3] if divpoint[2] == 0 else trace[divpoint[0]][2]
+                branch_dist = trace[divpoint[0]][3] if divpoint[
+                    2] == 0 else trace[divpoint[0]][2]
+
 #            fitness = app_lv + (1 - 1.001 ** (-branch_dist))
             fitness = [app_lv, branch_dist]
             self.dictionary[inputtuple] = fitness
             return fitness
-    
