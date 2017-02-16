c63dfde196f8c966c7b098074474f830e2559ff7
Junhwi Kim <junhwi.kim23@gmail.com>
2017-02-16 17:12:30 +0900

    Clean up build script

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ def parse_config(libs, lib_dirs, extra, include_dirs, flags):
 libs = []
 lib_dirs = []
 extra = []
+extra_link = []
 include_dirs = []
 
 if not os.environ.get('BINARY_DIR_PATH'):
@@ -49,7 +50,7 @@ proc = subprocess.Popen(
     [LLVM_BIN_PATH + '/llvm-config', '--ldflags', '--libs', '--system-libs'],
     stdout=subprocess.PIPE)
 LLVM_LDFLAGS = proc.communicate()[0].decode().split()
-parse_config(libs, lib_dirs, extra, include_dirs, LLVM_LDFLAGS)
+parse_config(libs, lib_dirs, extra_link, include_dirs, LLVM_LDFLAGS)
 
 LLVM_LDFLAGS_NOLIBS = LLVM_BIN_PATH + '/llvm-config --ldflags'
 PLUGIN_LDFLAGS = '-shared -Wl,-undefined,dynamic_lookup'
@@ -60,19 +61,19 @@ CLANG_INCLUDES = [
 ]
 
 CLANG_LIBS = [
-    '-lclangAST',
-    '-lclangAnalysis',
-    '-lclangBasic',
-    '-lclangDriver',
-    '-lclangEdit',
-    '-lclangFrontend',
-    '-lclangFrontendTool',
-    '-lclangLex',
-    '-lclangParse',
-    '-lclangSema',
-    '-lclangRewrite',
-    '-lclangTooling',
-    '-lclangSerialization',
+    'clangAST',
+    'clangAnalysis',
+    'clangBasic',
+    'clangDriver',
+    'clangEdit',
+    'clangFrontend',
+    'clangFrontendTool',
+    'clangLex',
+    'clangParse',
+    'clangSema',
+    'clangRewrite',
+    'clangTooling',
+    'clangSerialization',
 ]
 if platform.system() != 'Darwin':
     CLANG_LIBS = ['-Wl,--start-group'] + CLANG_LIBS + ['-Wl,--end-group']
@@ -83,8 +84,9 @@ module = Extension(
     depends=['lib/buildcfg.cpp', 'lib/consumer.cpp'],
     include_dirs=CLANG_INCLUDES + include_dirs,
     library_dirs=lib_dirs,
+    libraries=CLANG_LIBS,
     extra_compile_args=extra,
-    extra_link_args=CLANG_LIBS + libs)
+    extra_link_args=libs+extra_link)
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
