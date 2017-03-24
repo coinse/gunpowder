40454170063bf16ee5d5ff156603b9aeae3f37a6
bhyou <byou@kaist.ac.kr>
2017-03-24 16:03:17 +0900

    Support strcmp

--- util/strcmp2.c
+++ util/strcmp2.c
@@ -2,7 +2,7 @@
 double charDistance(char a, char b) {
     return 1.0 - 1.0 / (1.0 + abs(a - b));
 }
-int strcmp2(const char *p1, const char *p2)
+double strcmp2(const char *p1, const char *p2)
 {
     char *s1 = (char *) p1;
     char *s2 = (char *) p2;
@@ -11,7 +11,7 @@ int strcmp2(const char *p1, const char *p2)
     for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++) {
         distance += charDistance(*s1, *s2);
     }
-    lengthDiff = (int)abs(strlen(s1) - strlen(s2));
+    lengthDiff = (int)abs(strlen(p1) - strlen(p2));
     distance += lengthDiff;
     return distance;
 }
