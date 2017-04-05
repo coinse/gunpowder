0dcadb33126d245a45232e9a1fdcde222499d08c
bhyou <byou@kaist.ac.kr>
2017-04-05 15:51:16 +0900

    Fix strcmp2 distance

--- cavm/strcmp2.c
+++ cavm/strcmp2.c
@@ -1,17 +1,15 @@
 #include <string.h>
-double charDistance(char a, char b) {
-    return 1.0 - 1.0 / (1.0 + abs(a - b));
-}
-double strcmp2(const char *p1, const char *p2)
-{
-    char *s1 = (char *) p1;
-    char *s2 = (char *) p2;
-    int lengthDiff = 0;
-    double distance = 0;
-    for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++) {
-        distance += charDistance(*s1, *s2);
-    }
-    lengthDiff = abs((int)(strlen(p1) - strlen(p2)));
-    distance += lengthDiff;
-    return distance;
+int charDistance(char a, char b) { return abs(a - b); }
+double strcmp2(const char *p1, const char *p2) {
+  char *s1 = (char *)p1;
+  char *s2 = (char *)p2;
+  int lengthDiff = 0;
+  int charDiff = 0;
+  double distance = 0;
+  for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++) {
+    charDiff += charDistance(*s1, *s2);
+  }
+  lengthDiff = abs((int)(strlen(p1) - strlen(p2)));
+  distance = lengthDiff + (1 - pow(1.001, -charDiff));
+  return distance;
 }
